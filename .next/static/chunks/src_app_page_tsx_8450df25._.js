dfa09541a8afc226d17291ede83170323bca8ad3
Hans-Eirik <hbreyholtz@gmail.com>
2025-05-16 16:57:24 +0200

    downgrade react and react-dom dependencies to version 18.2.0 in package.json and package-lock.json, update related type definitions, and remove unused image cache files

--- .next/static/chunks/src_app_page_tsx_8450df25._.js
+++ .next/static/chunks/src_app_page_tsx_8450df25._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_9c7d184d._.js",
+  "static/chunks/node_modules_95d13c64._.js",
   "static/chunks/src_906bf2cd._.js",
   "static/chunks/node_modules_next_b69a028a._.js",
   "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
@@ -10,12 +10,12 @@
   "static/chunks/node_modules_motion_dist_es_motion-dom_dist_es_2f96890c._.js",
   "static/chunks/node_modules_motion_dist_es_motion_lib_react_mjs_6731964f._.js",
   "static/chunks/node_modules_motion_dist_es_motion-utils_dist_es_118d961d._.js",
-  "static/chunks/node_modules_framer-motion_dist_es_ad8b9672._.js",
-  "static/chunks/node_modules_motion-dom_dist_es_c7a0b6ce._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_1315c24f._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_fa3ea29e._.js",
   "static/chunks/node_modules_@radix-ui_react-icons_dist_react-icons_esm_ecd85ef9.js",
   "static/chunks/node_modules_@shikijs_24ac4549._.js",
   "static/chunks/node_modules_@radix-ui_e0cd37cd._.js",
-  "static/chunks/node_modules_8a5ad9c4._.js"
+  "static/chunks/node_modules_629930bb._.js"
 ],
     source: "dynamic"
 });
